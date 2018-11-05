396c62146647d9d742de6f86c927a56881966df1
Tiago Boeing <tiagoboeing@bol.com.br>
2018-11-05 01:17:39 -0200

    Mudança do app.api para cenário local

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,4 +1,4 @@
-export const API = 'https://desafio-senior-frontend-api.herokuapp.com';
+// export const API = 'https://desafio-senior-frontend-api.herokuapp.com';
 
 // se preferir rodar localmente com JSON-SERVER
-// export const API = 'http://localhost:3000';
+export const API = 'http://localhost:3000';
